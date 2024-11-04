306fe8a1cb4f2884a13e608e38f20ebf497eaec1
n.almohaimzy <noon.tk@hotmail.com>
2024-11-04 16:50:17 +0300

    refactor simlify games

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,5 +1,7 @@
 import { Heading } from '@chakra-ui/react'
 import { GameQuery } from '../App'
+import useGenres from './services/hooks/useGenres';
+import usePlatforms from './services/hooks/usePlatforms';
 
 
 interface Props {
@@ -7,7 +9,11 @@ interface Props {
 }
 
 const GameHeading = ({gameQuery}: Props) => {
-    const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`
+  const { data: genres} = useGenres();
+  const genre = genres?.results.find(g => g.id === gameQuery.genreId);
+  const { data: platforms } = usePlatforms();
+  const platform = platforms?.results.find(p => p.id === gameQuery.platformId);
+  const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
   return (
     <Heading as='h1' marginY={5} fontSize='5xl' textColor='#cbc0ac'>
         {heading}
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,10 +5,10 @@ import getCroppedImageUrl from './services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
+const GenreList = ({selectedGenreId, onSelectGenre}: Props) => {
     const {data, isLoading, error} = useGenres();
     if (error) return  null;
     if (isLoading) return  <Spinner />;
@@ -24,7 +24,17 @@ const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
               objectFit='cover'
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button whiteSpace='normal' textColor='gray.600' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+            <Button whiteSpace='normal' textColor='gray.600' textAlign='left' fontWeight={
+              genre.id === selectedGenreId
+               ? 'bold'
+               : 'normal'
+              }
+              onClick={()=> onSelectGenre(genre)}
+              fontSize='lg'
+              variant='link'
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>)}
       </List>
